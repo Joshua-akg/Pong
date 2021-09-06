332033ab7ca70aa664d4b9f9680240423bcc66c4
Joshua Akingbade <gbengaakingbade3@gmail.com>
2021-09-06 22:36:28 +0100

    Created ball class

--- main.cpp
+++ main.cpp
@@ -1,9 +1,61 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
+enum dir {STOP = 0, 
+                 LEFT = 1, 
+                 UPLEFT = 2, 
+                 DOWNLEFT = 3, 
+                 RIGHT = 4, 
+                 UPRIGHT = 5, 
+                 DOWNRIGHT = 6};
+
+class ball {
+    private:
+        int x, y;
+        int originalX, originalY;
+        dir direction;
+    
+    public:
+        ball(int posX, int posY) {
+            originalX = posX;
+            originalY = posY;
+            x = posX;
+            y = posY;
+        }
+
+        int getX() {
+            return x;
+        }
+
+        int getY() {
+            return y;
+        }
+
+        int getDirection() {
+            return direction;
+        }
+
+        void Reset() {
+            x = originalX;
+            y = originalY;
+            direction = STOP;
+        }
+
+        void changeDir(dir d) {
+            direction = d;
+        }
+
+        void changeRandomDir() {
+            direction = (dir)((rand() % 6) + 1)
+        }
+
+
+};
+
 int main() {
-    cout << "Hello world, this C++ works, DAMN!" << endl;    
+
+
+    
 
     return 0;
 }
