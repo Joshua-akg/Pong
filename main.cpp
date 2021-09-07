642fdc3d5224e3b2ceeb3df51ea4321f91e7cac6
Joshua Akingbade <gbengaakingbade3@gmail.com>
2021-09-07 12:47:13 +0100

    Created Paddle class

--- main.cpp
+++ main.cpp
@@ -2,87 +2,126 @@
 using namespace std;
 
 enum dir {STOP = 0, 
-                 LEFT = 1, 
-                 UPLEFT = 2, 
-                 DOWNLEFT = 3, 
-                 RIGHT = 4, 
-                 UPRIGHT = 5, 
-                 DOWNRIGHT = 6};
+          LEFT = 1, 
+          UPLEFT = 2, 
+          DOWNLEFT = 3, 
+          RIGHT = 4, 
+          UPRIGHT = 5, 
+          DOWNRIGHT = 6};
 
 class ball {
     private:
-        int x, y;
-        int originalX, originalY;
-        dir direction;
+    int x, y;
+    int originalX, originalY;
+    dir direction;
     
     public:
-        ball(int posX, int posY) {
-            originalX = posX;
-            originalY = posY;
-            x = posX;
-            y = posY;
+    ball(int posX, int posY) {
+        originalX = posX;
+        originalY = posY;
+        x = posX;
+        y = posY;
+        direction = STOP;
+    }
+
+    int getX() {
+        return x;
+    }
+
+    int getY() {
+        return y;
+    }
+
+    int getDirection() {
+        return direction;
+    }
+
+    void reset() {
+        x = originalX;
+        y = originalY;
+        direction = STOP;
+    }
+
+    void changeDir(dir d) {
+        direction = d;
+    }
+
+    void changeRandomDir() {
+        direction = (dir)((rand() % 6) + 1);
+    }
+
+    void move() {
+        switch (direction) {
+        case STOP:
+            break;
+        case LEFT:
+            x--;
+            break;
+        case RIGHT:
+            x++;
+            break;
+        case UPLEFT:
+            x--; y--;
+            break;
+        case DOWNLEFT:
+            x--; y++;
+            break;
+        case UPRIGHT:
+            x++; y--;
+            break;
+        case DOWNRIGHT:
+            x++; y++;
+            break;
+        default:
+            break;
         }
+    }
 
-        int getX() {
-            return x;
-        }
-
-        int getY() {
-            return y;
-        }
-
-        int getDirection() {
-            return direction;
-        }
-
-        void Reset() {
-            x = originalX;
-            y = originalY;
-            direction = STOP;
-        }
-
-        void changeDir(dir d) {
-            direction = d;
-        }
-
-        void changeRandomDir() {
-            direction = (dir)((rand() % 6) + 1)
-        }
-
-        void Movement() {
-            switch (direction) {
-            case STOP:
-                break;
-            case LEFT:
-                x--;
-                break;
-            case RIGHT:
-                x++;
-                break;
-            case UPLEFT:
-                x--; y--;
-                break;
-            case DOWNLEFT:
-                x--; y++;
-                break;
-            case UPRIGHT:
-                x++; y--;
-                break;
-            case DOWNRIGHT:
-                x++; y++;
-                break;
-            default:
-                break;
-            }
-        }
-
-
+    friend ostream & operator<<(ostream & o, ball c) {
+        o << "Ball [" << c.x << "," << c.y << "][" << c.direction << "]";
+        return o;
+    }
 };
 
-int main() {
+class paddle {
+private:
+    int x, y;
+    int originalX, originalY;
+    
+public:
+    paddle() {
+        x = 0;
+        y = 0; 
+    }
 
+    paddle(int xPos, int yPos) : paddle() {
+        originalX = xPos;
+        originalY = yPos;
+        x = xPos;
+        y = yPos; 
+    }
+
+    void reset() {
+        x = originalX; 
+        y = originalY;
+    }
 
     
+
+}
+
+int main() {
+    // test behaviour
+    ball b(0, 0);
+    cout << b << endl;
+    b.changeRandomDir();
+    cout << b << endl;
+    b.move();
+    cout << b << endl;
+
+    b.changeRandomDir();
+    b.move();
+    cout << b << endl;
 
     return 0;
 }
